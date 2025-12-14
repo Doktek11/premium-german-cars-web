446c7cd0c0f061d1e010eda4139e4d59a25a4d5c
Doktek11 <thedoctor135@gmail.com>
2025-12-14 12:48:07 +0100

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,34 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-import { Home } from "./Home";
-import { CarPage } from "./pages/CarPage";
-import { ImportacionAlemania } from "./pages/ImportacionAlemania";
-import { AvisoLegal } from "./pages/AvisoLegal";
-import { PoliticaPrivacidad } from "./pages/PoliticaPrivacidad";
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* HOME */}
-        <Route path="/" element={<Home />} />
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-        {/* FICHA VEHÍCULO */}
-        <Route path="/car/:slug" element={<CarPage />} />
-
-        {/* IMPORTACIÓN ALEMANIA */}
-        <Route
-          path="/importacion-coches-alemania"
-          element={<ImportacionAlemania />}
-        />
-
-        {/* LEGALES (NOINDEX) */}
-        <Route path="/aviso-legal" element={<AvisoLegal />} />
-        <Route
-          path="/politica-de-privacidad"
-          element={<PoliticaPrivacidad />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  return null;
+};

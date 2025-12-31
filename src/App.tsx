a4c77a72c77ac9fb7889f1b22ffbb33c72d8104e
Doktek11 <thedoctor135@gmail.com>
2025-12-31 18:14:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Componentes Principales
 import { Home } from "./Home";
 import { CarPage } from "./pages/CarPage";
 import { ImportacionAlemania } from "./pages/ImportacionAlemania";
+import ScrollToTop from "./ScrollToTop"; // Importación desde el archivo externo para mayor limpieza
 
 // IMPORTACIONES DEL BLOG
 import BlogIndex from "./pages/Blog/index";
@@ -11,38 +12,29 @@ import MotoresBmwMercedes2027 from "./pages/Blog/motores-bmw-en-mercedes-2027";
 import BmwReestreno2026 from "./pages/Blog/bmw-reestreno-alemania-2026";
 import CochesReusTarragona from "./pages/Blog/coche-segunda-mano-reus-tarragona";
 
-// ✅ Componente interno para forzar el scroll al inicio en cada cambio de ruta
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
 export default function App() {
   return (
     <BrowserRouter>
-      {/* Ejecuta el scroll automático */}
+      {/* Forzamos el scroll al inicio en cada cambio de URL */}
       <ScrollToTop />
       
       <Routes>
-        {/* HOME */}
+        {/* LANDING PRINCIPAL */}
         <Route path="/" element={<Home />} />
 
-        {/* FICHA VEHÍCULO */}
+        {/* FICHA VEHÍCULO INDIVIDUAL */}
         <Route path="/car/:slug" element={<CarPage />} />
 
-        {/* IMPORTACIÓN ALEMANIA */}
+        {/* PÁGINA DE SERVICIO DE IMPORTACIÓN */}
         <Route
           path="/importacion-coches-alemania"
           element={<ImportacionAlemania />}
         />
 
-        {/* RUTAS DEL BLOG */}
+        {/* SECCIÓN BLOG - LISTADO */}
         <Route path="/blog" element={<BlogIndex />} />
+        
+        {/* ARTÍCULOS INDIVIDUALES DEL BLOG */}
         <Route 
           path="/blog/motores-bmw-en-mercedes-2027" 
           element={<MotoresBmwMercedes2027 />} 
@@ -55,6 +47,8 @@ export default function App() {
           path="/blog/coche-segunda-mano-reus-tarragona" 
           element={<CochesReusTarragona />} 
         />
+
+        {/* Puedes añadir una ruta de captura 404 aquí en el futuro si lo deseas */}
       </Routes>
     </BrowserRouter>
   );

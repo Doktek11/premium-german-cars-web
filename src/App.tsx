12eaf3b616029d84871c84b6ca9868a18647bf23
Doktek11 <thedoctor135@gmail.com>
2026-01-02 11:00:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Home } from "./Home";
 import { CarPage } from "./pages/CarPage";
 import { ImportacionAlemania } from "./pages/ImportacionAlemania";
+import { CalculadoraImpuestos } from "./pages/CalculadoraImpuestos"; // <-- Nueva importación
 
 // IMPORTACIONES DEL BLOG
 import BlogIndex from "./pages/Blog/index";
-import ArticuloModelos2026 from "./pages/Blog/ArticuloModelos2026"; // <-- Nueva importación
+import ArticuloModelos2026 from "./pages/Blog/ArticuloModelos2026";
 import MotoresBmwMercedes2027 from "./pages/Blog/motores-bmw-en-mercedes-2027";
 import BmwReestreno2026 from "./pages/Blog/bmw-reestreno-alemania-2026";
 import CochesReusTarragona from "./pages/Blog/coche-segunda-mano-reus-tarragona";
@@ -17,46 +18,40 @@ import CochesReusTarragona from "./pages/Blog/coche-segunda-mano-reus-tarragona"
 import AvisoLegal from "./pages/Legal/AvisoLegal";
 import PoliticaPrivacidad from "./pages/Legal/PoliticaPrivacidad";
 
-// ✅ Definimos ScrollToTop aquí dentro para evitar errores de "Could not resolve"
 function ScrollToTop() {
   const { pathname } = useLocation();
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-
   return null;
 }
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* Forzamos el scroll al inicio en cada cambio de URL */}
       <ScrollToTop />
       
       <Routes>
-        {/* LANDING PRINCIPAL */}
         <Route path="/" element={<Home />} />
-
-        {/* FICHA VEHÍCULO INDIVIDUAL */}
         <Route path="/car/:slug" element={<CarPage />} />
-
-        {/* PÁGINA DE SERVICIO DE IMPORTACIÓN */}
+        
         <Route
           path="/importacion-coches-alemania"
           element={<ImportacionAlemania />}
         />
 
-        {/* SECCIÓN BLOG - LISTADO */}
+        {/* NUEVA RUTA CALCULADORA */}
+        <Route 
+          path="/calculadora-impuesto-matriculacion" 
+          element={<CalculadoraImpuestos />} 
+        />
+
         <Route path="/blog" element={<BlogIndex />} />
         
-        {/* ARTÍCULOS INDIVIDUALES DEL BLOG */}
-        {/* Nuevo Artículo Destacado 2026 */}
         <Route 
           path="/blog/mejores-modelos-importar-alemania-2026" 
           element={<ArticuloModelos2026 />} 
         />
-
         <Route 
           path="/blog/motores-bmw-en-mercedes-2027" 
           element={<MotoresBmwMercedes2027 />} 
@@ -70,13 +65,10 @@ export default function App() {
           element={<CochesReusTarragona />} 
         />
 
-        {/* PÁGINAS LEGALES */}
         <Route path="/aviso-legal" element={<AvisoLegal />} />
         <Route path="/politica-privacidad" element={<PoliticaPrivacidad />} />
 
-        {/* Ruta opcional por si alguien escribe mal una URL (Catch-all) */}
         <Route path="*" element={<Home />} />
-
       </Routes>
     </BrowserRouter>
   );

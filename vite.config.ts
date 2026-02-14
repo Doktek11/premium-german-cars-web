8f9de9286c882c1b51acc1ccc03fab380271f4fc
Doktek11 <thedoctor135@gmail.com>
2026-02-14 22:10:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 450,
+    outDir: "dist",
+    manifest: true,
+    chunkSizeWarningLimit: 350,
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['lucide-react', 'react-helmet-async'],
+          react: ["react", "react-dom", "react-router-dom"],
+          ui: ["lucide-react", "react-helmet-async"],
         },
       },
     },
